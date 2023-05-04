512bb9df2bc059e9244382afc1c63ebc2ce6b3da
Ryebread5555 <Rypetty55@gmail.com>
2023-05-03 21:06:24 -0500

    added userSchema and hashing for user password

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must use a valid email address'],
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        // used to select a role on user profile
+        role: {
+            type: String,
+            enum: ['service worker', 'pet owner'],
+            required: true
+        }
+    }
+);
+
+// create hash for user password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+
+// compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
