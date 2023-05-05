9373ccaa1bf67c3d95197bab5fd35a8612f494d2
Ryebread5555 <Rypetty55@gmail.com>
2023-05-04 19:24:16 -0500

    created petSchema

---
+++ server/models/Pet.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+const petSchema = new Schema(
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      
+      breed: {
+        type: String,
+        required: true,
+      },
+
+      age: {
+        type: Number,
+        required: true,
+      },
+
+      type: {
+        type: String,
+        enum: ['dog', 'cat', 'bird', 'guinea pig', 'exotic'],
+        required: true,
+      },
+
+      owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+      },
+
+      image: {
+        type: String,
+      },
+    },
+  );
+  
+
+module.exports = model('Pet', petSchema);
+
