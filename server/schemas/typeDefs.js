c49e8a74c184292ac000f24f6560c7426cea29fa
Ryebread5555 <Rypetty55@gmail.com>
2023-05-08 21:15:33 -0500

    added checkout to query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,6 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-
   type User {
     _id: ID
     username: String!
@@ -46,6 +45,7 @@ const typeDefs = gql`
     order(_id: ID!): Order
     user: User
     pet: Pet
+    checkout: String
   }
 
   type Mutation {
