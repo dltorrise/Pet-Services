dfa955592c14df9de5c3cc7fcc7b0c317bf5b7a5
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-05-10 22:06:56 -0400

    fixed apollo provider index.js on client

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import App from "./App";
 import Cart from "./components/pages/Cart";
 import Login from "./components/pages/Login"
 import Profile from "./components/pages/Profile"
 import Signup from "./components/pages/Signup"
-import { ApolloProvider } from "@apollo/client";
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 export default function Index() {
 return (
 <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-        <Route path= 'cart' element={<Cart />} />
-        <Route path= 'login' element={<Login />} />
-        <Route path= 'profile' element={<Profile />} />
-        <Route path= 'sign-up' element={<Signup />} />
-      </Routes>
+      <ApolloProvider client={client}>
+        <Routes>
+          <Route index element={<App />} />
+          <Route path= 'cart' element={<Cart />} />
+          <Route path= 'login' element={<Login />} />
+          <Route path= 'profile' element={<Profile />} />
+          <Route path= 'signup' element={<Signup />} />
+        </Routes>
+      </ApolloProvider>
     </BrowserRouter>
 );
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Index/>);
-
-
 
