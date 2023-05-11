29961c83bab8b34ba3ad9b4abea8f5d905235d48
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-05-10 20:39:11 -0400

    added signin and signup pages, added stripe, jwt, and apollo npm scripts to package json

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -2,6 +2,6 @@
 export const TOGGLE_CART = "TOGGLE_CART"; 
 export const ADD_TO_CART = "ADD_TO_CART";
 export const ADD_MULTIPLE_TO_CART = "ADD_MULTIPLE_TO_CART";
-export const UPDATE_CART_QUANTITY = "UPDATE_CART_QUANTITY"; // check on this if we need
+export const UPDATE_CART_QUANTITY = "UPDATE_CART_QUANTITY";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART"; // for one item
 export const CLEAR_CART = "CLEAR_CART"; // clear whole cart at ones
