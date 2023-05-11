32b04ba679c87c6745608888c6224c4775662dee
dltorrise <dltorrise@gmail.com>
2023-05-11 18:21:50 -0500

    added to routes and app.jsx

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import AuthService from './utils/auth'
 import {
   ApolloClient,
   InMemoryCache,
@@ -14,10 +15,6 @@ import Cart from "./components/pages/Cart";
 import Login from "./components/pages/Login"
 import Profile from "./components/pages/Profile"
 import Signup from "./components/pages/Signup"
-<<<<<<< HEAD
-import { ApolloProvider } from "@apollo/client";
-import AuthService from './utils/auth'
-=======
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -37,30 +34,19 @@ const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
->>>>>>> 85030ad6a3ba4932e3a90bb5bd20b047e81f337d
 
 export default function Index() {
 return (
 <BrowserRouter>
-<<<<<<< HEAD
-      <Routes>
-        <Route index element={<App />} />
-        <Route { AuthService.loggedIn() ? path= 'cart' : path='login'} element={<Cart />} />
-        <Route path= 'login' element={<Login />} />
-        <Route { AuthService.loggedIn() ? path= 'profile' : path='login'} element={<Profile />} />
-        <Route path= 'sign-up' element={<Signup />} />
-      </Routes>
-=======
       <ApolloProvider client={client}>
         <Routes>
           <Route index element={<App />} />
-          <Route path= 'cart' element={<Cart />} />
+          <Route path= 'cart' element={ AuthService.loggedIn() ? <Cart /> : <Navigate to="/login"/> } />
+          <Route path= 'profile' element={ AuthService.loggedIn() ? <Profile /> : <Navigate to="/login"/> } />
           <Route path= 'login' element={<Login />} />
-          <Route path= 'profile' element={<Profile />} />
           <Route path= 'signup' element={<Signup />} />
         </Routes>
       </ApolloProvider>
->>>>>>> 85030ad6a3ba4932e3a90bb5bd20b047e81f337d
     </BrowserRouter>
 );
 }
